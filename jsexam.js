e7152f7350c3d4a4ca9d1edb55b35009db51af99
윤구 이 <dldbsrn0120@naver.com>
2020-02-07 00:50:13 +0900

    second commit

--- jsexam.js
+++ jsexam.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+var arr = [1,2,3,4,5];
+
+var familyClass = {
+    members: {},
+    addFamily: function(age, name, role) {
+        this.members[role] = {
+            age: age,
+            name: name
+        };
+    },
+
+    getHeadCount: function() {
+        return Object.keys(this.members).length;
+    }
+};
