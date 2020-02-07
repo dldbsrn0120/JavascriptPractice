d7705506a3feebecc1cf7f1c6db7563542503a4b
dldbsrn0120 <dldbsrn0120@gmail.com>
2020-02-08 08:44:23 +0900

    class

---
+++ js_006.js
@@ -0,0 +1,22 @@
+// class constructor
+// 006.js
+
+function Teacher(name, age, subject) {
+    this.name = name;
+    this.age = age;
+    this.subject = subject;
+    this.teach = function (student) {
+        console.log(student + '에게 ' + this.subject + '를 가르칩니다.');
+    };
+}
+
+const jay = new Teacher('jay', 30, 'javascript');   // constructor
+console.log(jay);
+jay.teach('bbo');
+
+console.log(jay.constructor);
+console.log(jay instanceof Teacher);
+
+const jay2 = Teacher('jay', 20, 'css');     // function
+console.log(jay2);
+console.log(age);
